6f403de5a809918f837ef1ad0f5259060750cb66
Giuseppe Varriale <pepe.saoleo@gmail.com>
2024-12-07 12:20:53 -0300

    feat: initial commit

---
+++ src/main/java/com/aguasfluentessa/pressuremonitor/PressuremonitorApplication.java
@@ -0,0 +1,13 @@
+package com.aguasfluentessa.pressuremonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressuremonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PressuremonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aguasfluentessa/pressuremonitor/PressuremonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguasfluentessa.pressuremonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressuremonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
