b5b631d6fe8865e36fa62eeacaa3c458cc9e5e73
Giuseppe Varriale <pepe.saoleo@gmail.com>
2024-12-08 07:55:24 -0300

    chore: remove unused imports

--- src/main/java/com/aguasfluentessa/pressuremonitor/model/PressureGauge.java
+++ src/main/java/com/aguasfluentessa/pressuremonitor/model/PressureGauge.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.CurrentTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
