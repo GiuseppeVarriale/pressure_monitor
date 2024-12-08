8c2846266ca3fe003f64fd28e72069bcf0158625
Giuseppe Varriale <pepe.saoleo@gmail.com>
2024-12-08 08:02:36 -0300

    chore: remove unused imports

--- src/main/java/com/aguasfluentessa/pressuremonitor/model/PressureGauge.java
+++ src/main/java/com/aguasfluentessa/pressuremonitor/model/PressureGauge.java
@@ -2,10 +2,8 @@ package com.aguasfluentessa.pressuremonitor.model;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
-
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
--- src/test/java/com/aguasfluentessa/pressuremonitor/repository/PressureGaugeRepositoryTest.java
+++ src/test/java/com/aguasfluentessa/pressuremonitor/repository/PressureGaugeRepositoryTest.java
@@ -5,7 +5,6 @@ import com.aguasfluentessa.pressuremonitor.model.PressureGauge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 
