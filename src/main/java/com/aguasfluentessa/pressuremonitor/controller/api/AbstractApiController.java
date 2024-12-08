e5664a2355d593d997e856b27deeac3f79868a6e
Giuseppe Varriale <pepe.saoleo@gmail.com>
2024-12-08 09:42:19 -0300

    chore: remove unused import

--- src/main/java/com/aguasfluentessa/pressuremonitor/controller/api/AbstractApiController.java
+++ src/main/java/com/aguasfluentessa/pressuremonitor/controller/api/AbstractApiController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.aguasfluentessa.pressuremonitor.model.Exception.PressureGaugeNotFoundException;
-
 @RestController
 @RequestMapping("/api")
 public abstract class AbstractApiController {
