07fd0aa9b521be4fc9fab4319b440ad57fc8e580
Giuseppe Varriale <pepe.saoleo@gmail.com>
2024-12-09 09:09:19 -0300

    feat: add connection factory to redis config

--- src/main/java/com/aguasfluentessa/pressuremonitor/config/RedisConfig.java
+++ src/main/java/com/aguasfluentessa/pressuremonitor/config/RedisConfig.java
@@ -2,13 +2,21 @@ package com.aguasfluentessa.pressuremonitor.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@Profile("!test")
 public class RedisConfig {
+
+      @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
@@ -16,6 +24,7 @@ public class RedisConfig {
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+       
         return template;
     }
 }
